0d377d8ec18b4a9f5c36d35389ba75db2e1a8736
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-16 12:17:45 +0000

    added setpass.sh

---
+++ setpass.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set redis password to first argument
+
+if [ $# != 1 ]
+then 
+  echo Must give password as first argument
+  exit 1
+fi
+
+echo "CONFIG SET requirepass $1" | nc -w 1 127.0.0.1 6379
